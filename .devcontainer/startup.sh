ffd99128947c93b960c105dd9a0a0fab938e085b
Floris van Doorn <fpvdoorn@gmail.com>
2023-07-08 19:38:26 +0200

    remove obsolete file

--- .devcontainer/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "Downloading the Lean 4 mathematical library..."
-lake exe cache get!
-lake build +LCTutorial.Library.Basic
-echo "Finished setup! Open a file using the Explorer in the top-left of your screen."
